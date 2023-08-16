6996d3e69213282cb288acbc1696f5fed85fd93b
Peter Krautzberger <p.krautzberger@gmail.com>
2023-08-16 10:48:33 +0200

    fix(update_ams.sh): add new branches
    Should fix 3 bugs we reported.

--- amermathsoc/update_ams.sh
+++ amermathsoc/update_ams.sh
@@ -3,7 +3,7 @@
 echo "running git fetch"
 git fetch mathjax
 
-for p in "develop" "issue3082" "issue3070"
+for p in "develop" "issue3082" "issue3070" "issue3083" "issue3084" "issue3085" 
 do
   echo "merging mathjax/$p"
   git merge mathjax/"$p"
